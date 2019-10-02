471ed757724e4eb5f1dfb4e41e1f26210119953d
VladOvinnikov <vlad.ovinnikov@gmail.com>
2019-10-02 15:49:22 +0200

    update ListResponse by total

--- src/containers.rs
+++ src/containers.rs
@@ -3,6 +3,7 @@ use ::serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ListResponse<T> {
+    pub total: i64,
     pub total_pages: i64,
     pub data: Vec<T>
 }
