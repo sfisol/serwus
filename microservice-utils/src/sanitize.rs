925951eca02cabe0c5dd8faf62d9463c608129d7
Michał Pokrywka <wolfmoon@o2.pl>
2023-05-11 10:48:33 +0200

    Fixed using sanitizable twice in the same scope

--- microservice-utils/src/sanitize.rs
+++ microservice-utils/src/sanitize.rs
@@ -5,8 +5,7 @@ pub trait Sanitize {
 #[macro_export]
 macro_rules! sanitizable {
     ($model:ident, $( $field:ident ),* ) => {
-        use $crate::Sanitize;
-        impl Sanitize for $model {
+        impl $crate::Sanitize for $model {
             fn sanitize(&mut self) {
                 $(self.$field = self.$field.trim().to_string();)*
             }
