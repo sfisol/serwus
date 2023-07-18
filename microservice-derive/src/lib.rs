19f284de8bd60947cadcfd7bdc88a1c3c75b49cc
Michał Pokrywka <wolfmoon@o2.pl>
2023-07-18 20:37:18 +0200

    Added JsonError, ErrorBuilder

--- microservice-derive/src/lib.rs
+++ microservice-derive/src/lib.rs
@@ -1,11 +1,10 @@
-// Empty implementation for statsPresenter
-
 #![deny(rust_2018_idioms)]
 #![warn(clippy::all)]
 
 use proc_macro::TokenStream;
 use quote::quote;
 
+// Empty implementation for statsPresenter
 #[proc_macro_derive(EmptyStats)]
 pub fn empty_stats_macro_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
@@ -42,10 +41,27 @@ fn impl_canceled_macro(ast: &syn::DeriveInput) -> TokenStream {
         impl ::std::convert::From<::actix_web::error::BlockingError> for #name {
             fn from(b_err: ::actix_web::error::BlockingError) -> Self {
                 Self::Canceled
-                // match b_err {
-                //     ::actix_web::error::BlockingError::Canceled => Self::Canceled,
-                //     ::actix_web::error::BlockingError::Error(err) => err,
-                // }
+            }
+        }
+    };
+    gen.into()
+}
+
+/// Implements `ResponseError` for struct if it implements Into<ErrorBuilder>
+#[proc_macro_derive(ResponseFromBuilder)]
+pub fn response_from_builder_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_response_from_builder(&ast)
+}
+
+fn impl_response_from_builder(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl ::actix_web::ResponseError for #name {
+            fn error_response(&self) -> ::actix_web::HttpResponse {
+                ::microservice::server::json_error::ErrorBuilder::from(self)
+                    .finish()
+                    .error_response()
             }
         }
     };
