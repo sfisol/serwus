cd8d21e24f2a270704234e7384939b3a84a20fa9
Michał Pokrywka <wolfmoon@o2.pl>
2023-07-20 20:43:23 +0200

    non-generic generic error

--- src/server/generic_error.rs
+++ src/server/generic_error.rs
@@ -1,3 +1,4 @@
+use actix_http::body::MessageBody;
 use actix_web::{
     dev::ServiceResponse,
     http::{header, StatusCode},
@@ -7,21 +8,25 @@ use actix_web::{
     Result,
 };
 use derive_more::Display;
-use serde::{Serialize, Serializer};
+use serde::Serialize;
 use std::fmt::{Debug, Display};
 
+pub use microservice_derive::ResponseFromBuilder;
+
 #[derive(Debug, Display, Serialize)]
-pub enum GenericErrorType<T> {
+#[cfg_attr(feature = "swagger", derive(paperclip::actix::Apiv2Schema))]
+pub enum GenericErrorType {
     BadRequest,
     NotFound,
     Internal,
     Other,
+    Database,
     ValidationFail,
     InvalidParams,
-    Custom(T),
+    Custom,
 }
 
-impl From<StatusCode> for GenericErrorType<()> {
+impl From<StatusCode> for GenericErrorType {
     fn from(value: StatusCode) -> Self {
         if value == StatusCode::NOT_FOUND {
             Self::NotFound
@@ -36,21 +41,112 @@ impl From<StatusCode> for GenericErrorType<()> {
 }
 
 #[derive(Debug, Display, Serialize)]
+#[cfg_attr(feature = "swagger", derive(paperclip::actix::Apiv2Schema))]
 #[display(fmt = "{} ({}) {}", status_code, r#type, message)]
-pub struct GenericError<T> {
-    #[serde(serialize_with = "serialize_status_code")]
-    pub status_code: StatusCode,
-    pub r#type: GenericErrorType<T>,
+pub struct GenericError {
+    // Skip `status_code` field to allow to derive Apiv2Schema without wrapping StatusCode type;
+    // document and serialize field `status` instead.
+    // Keep them private to assure consistency
+    #[serde(skip)]
+    status_code: StatusCode,
+    status: u16,
+    /// Generic type common to all services
+    pub r#type: GenericErrorType,
+    /// Message to be displayed to the user
     pub message: String,
+    /// Error representation for tracing exact place in the code where error occurred
+    pub debug: Option<String>,
+    /// Detailed reason of the error
+    pub reason: String,
+    /// Any additional data, needs to be used when presenting error to the user (f. ex. validation errors)
+    pub data: Option<String>,
 }
 
-impl<T> ResponseError for GenericError<T>
-where
-    T: Debug + Display + Serialize
-{
+pub const GENERIC_MESSAGE: &str = "Something went wrong. Try again later";
+pub const GENERIC_REASON: &str = "Unknown";
+
+pub struct ErrorBuilder {
+    inner: GenericError
+}
+
+impl ErrorBuilder {
+    // Starters
+    pub fn new(status_code: StatusCode, r#type: GenericErrorType, reason: impl Display) -> ErrorBuilder {
+        let reason = reason.to_string();
+
+        Self {
+            inner: GenericError {
+                status: status_code.as_u16(),
+                status_code,
+                r#type,
+                message: GENERIC_MESSAGE.to_string(),
+                debug: None,
+                reason,
+                data: None,
+            }
+        }
+    }
+
+    pub fn internal(reason: impl Display) -> Self {
+        let status_code = StatusCode::INTERNAL_SERVER_ERROR;
+        Self::new(status_code, GenericErrorType::Internal, reason)
+    }
+
+    pub fn database(reason: impl Display) -> Self {
+        let status_code = StatusCode::INTERNAL_SERVER_ERROR;
+        Self::new(status_code, GenericErrorType::Database, reason)
+    }
+
+    pub fn custom(reason: impl Display) -> Self {
+        let status_code = StatusCode::INTERNAL_SERVER_ERROR;
+        Self::new(status_code, GenericErrorType::Custom, reason)
+    }
+
+    // Modifiers
+    pub fn status(mut self, status_code: StatusCode) -> Self {
+        self.inner.status = status_code.as_u16();
+        self.inner.status_code = status_code;
+        self
+    }
+
+    pub fn message(mut self, message: impl Display) -> Self {
+        self.inner.message = message.to_string();
+        self
+    }
+
+    pub fn r#type(mut self, r#type: GenericErrorType) -> Self {
+        self.inner.r#type = r#type;
+        self
+    }
+
+    pub fn debug(mut self, debug: impl Debug) -> Self {
+        self.inner.debug = Some(format!("{:?}", debug));
+        self
+    }
+
+    pub fn data(mut self, data: impl Serialize) -> Self {
+        let data = serde_json::to_string(&data)
+            .or_else(|err| {
+                let err_str = err.to_string();
+                log::error!("Error serializing error: {}", err_str);
+                Ok::<_, ()>(err_str)
+            })
+            .ok();
+
+        self.inner.data = data;
+        self
+    }
+
+    // Produce ready error
+    pub fn finish(self) -> GenericError {
+        self.inner
+    }
+}
+
+impl ResponseError for GenericError {
     fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
         if self.status_code > StatusCode::INTERNAL_SERVER_ERROR {
-            log::error!("{}", self);
+            log::error!("{} (reason: {})", self, self.reason);
         }
         HttpResponse::build(self.status_code)
             .content_type("application/json; charset=utf-8")
@@ -62,32 +158,49 @@ where
     }
 }
 
-fn serialize_status_code<S>(value: &StatusCode, ser: S) -> Result<S::Ok, S::Error>
-where S: Serializer
-{
-    ser.serialize_u16(value.as_u16())
-}
-
-pub fn default_error_handler<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
+/// Middleware for converting plain actix errors into JSON ones with GenericError schema
+pub fn default_error_handler<B: MessageBody + 'static>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
+    // Disassemble service response
     let (req, mut response) = res.into_parts();
 
-    let status_code = response.status();
-    let r#type = GenericErrorType::from(status_code);
-    let message = response.error().map(|err| err.to_string()).unwrap_or_else(|| status_code.to_string());
+    let json_content_type  = header::HeaderValue::from_static("application/json; charset=utf-8");
 
-    let err = GenericError {
-        status_code,
-        r#type,
-        message,
+    // Rewrite response only if it is an error, but not JSON already
+    let res = if !response.status().is_success() && response.headers().get(header::CONTENT_TYPE) != Some(&json_content_type) {
+        let status_code = response.status();
+        let r#type = GenericErrorType::from(status_code);
+
+        // Happily error message can be taken from "special place" in response struct,
+        // not necessarily from the body which requires waiting.
+        // If error message is not provided, take it from status code.
+        // TODO: Maybe sometimes it would be better to place this in 'reason' but how to distinguish when?
+        let message = response.error()
+            .map(|err| err.to_string())
+            .unwrap_or_else(|| status_code.to_string());
+
+        let debug = "default_error_handler".to_string();
+        let reason = "".to_string();
+
+        let err = GenericError {
+            status: status_code.as_u16(),
+            status_code,
+            r#type,
+            message,
+            debug: Some(debug),
+            reason,
+            data: None,
+        };
+
+        // Overwrite response content-type
+        response.headers_mut().insert(header::CONTENT_TYPE, json_content_type);
+
+        // Overwrite response body
+        response.set_body(serde_json::to_string(&err).unwrap()).map_into_boxed_body()
+    } else {
+        // Leave response intact
+        response.map_into_boxed_body()
     };
 
-    response.headers_mut().insert(
-        header::CONTENT_TYPE,
-        header::HeaderValue::from_static("application/json; charset=utf-8"),
-    );
-
-    let res = response.set_body(serde_json::to_string(&err).unwrap()).map_into_boxed_body();
     let res = ServiceResponse::new(req, res).map_into_right_body();
-
     Ok(ErrorHandlerResponse::Response(res))
 }
